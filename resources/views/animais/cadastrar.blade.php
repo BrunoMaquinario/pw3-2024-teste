b848fbc1943b15d0510e9783f9a718861ac4fa96
Bruno <104913460+BrunoMaquinario@users.noreply.github.com>
2024-09-03 09:01:43 -0300

    Aula 03/09 - Implementação da coluna Imagem

--- resources/views/animais/cadastrar.blade.php
+++ resources/views/animais/cadastrar.blade.php
@@ -16,12 +16,20 @@
 </div>  
 @endif
 
-<form method="post" action="{{ route('animais.gravar') }}">
-    @csrf
-    <input type="text" name="nome" placeholder="Nome" value="{{ old('nome') }}">
-    <br>
-    <input type="number" name="idade" placeholder="Idade" value="{{ old('idade') }}">
-    <br>
-    <input type="submit" value="Gravar">
-</form>
-@endsection
+<div class="leading-loose">
+    <form method="post" enctype="multipart/form-data" action="{{ route('animais.gravar') }}" class="p-10 bg-white rounded shadow-xl">
+        @csrf
+            <label class="block text-sm text-gray-600" for="name">Name</label>
+            <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" type="text" name="nome" placeholder="Nome" value="{{ old('nome') }}">
+        <br>
+            <label class="block text-sm text-gray-600" for="idade">Idade</label>
+            <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" type="number" name="idade" placeholder="Idade" value="{{ old('idade') }}">
+        <br>
+            <label class="block text-sm text-gray-600" for="imagem">Imagem</label>
+            <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" type="file" name="imagem" placeholder="Imagem" value="{{ old('imagem') }}">
+        <br>
+        <input type="submit" value="Gravar">
+    </form>
+    </div>
+    @endsection
+    
